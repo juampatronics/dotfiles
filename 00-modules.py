cb81edfde3fec4f9085554c04d3e4eb6319dd3c6
Juampa <1523654+juampatronics@users.noreply.github.com>
2021-04-01 09:30:44 +0200

    autoreload on startup

--- 00-modules.py
+++ 00-modules.py
@@ -9,5 +9,14 @@ print(f'pandas {pd.__version__}')
 print(f'matplotlib {mpl.__version__}')
 
 print('Automatically reload all modules...')
-%load_ext autoreload
-%autoreload 2
+
+# we need to reqrite these two lines programatically...
+# %load_ext autoreload
+# %autoreload 2
+
+from IPython import get_ipython
+ipython = get_ipython()
+
+# ipython.magic("pylab")
+ipython.magic("load_ext autoreload")
+ipython.magic("autoreload 2")
